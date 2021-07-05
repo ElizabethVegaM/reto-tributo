c6e4d4e425c53426d7fe17b243343c0a4b65b3eb
Elizabeth Vega <eb.vega.m@gmail.com>
2021-07-05 16:26:11 -0400

    adds basic styles

--- src/App.js
+++ src/App.js
@@ -3,33 +3,37 @@ import { timeline } from './timeline';
 import ListItem from './ListItem';
 
 function App() {
+  const switchButton = {
+    
+
+  }
   return (
-    <>
-      <header>
+    <div id="container" className="light-theme">
+      <header id="header-container">
         <h1>Marie Curie Tribute Page</h1>
-        <p>The first woman to win a Nobel Prize. She made many discoveries that led to what we call modern medicine</p>
-        <button className="light-theme">Dark</button>
+        <p>The first woman to win a Nobel Prize. She made many discoveries that led to what we call modern medicine.</p>
+        <button id="theme-button" className="dark-theme">Dark</button>
       </header>
-      <main>
-        <figure>
+      <main id="main-section">
+        <figure id="img-container">
           <img src="http://www.uncuyo.edu.ar/ices/cache/curie_800_900.jpg" alt="Marie Curie in her laboratory" />
           <figcaption>
           Maria Salomea Skłodowska-Curie, known as Marie Curie, poses at the laboratory in Paris.
           </figcaption>
         </figure>
-        <section>
-          <h3>This is a timeline of Marie Curie's life:</h3>
-          <ul>
+        <section id="timeline-section">
+          <h3 id="timeline-title">This is a timeline of <span>Marie Curie's</span> life:</h3>
+          <ul id="timeline-list">
           {timeline.map((el) => <ListItem date={el.date} text={el.text} />)}
           </ul>
           <p>Joliot-Curie’s daughter became a nuclear physicist and their son a biochemist, both making significant scientific contributions. Their grandson is an astrophysicist.</p>
           <h3>
-          If you have time, you should read more about this incredible woman on her
+          If you have time, you should read more about this incredible woman on her 
           <a href="https://en.wikipedia.org/wiki/Marie_Curie" target="_blank">Wikipedia entry</a>.
         </h3>
         </section>
       </main>
-    </>
+    </div>
   );
 }
 
--- src/timeline.js
+++ src/timeline.js
@@ -1,10 +1,10 @@
 export const timeline = [
   {
-    'date': 1867,
+    'date': '1867',
     'text': 'Born in Warsaw, Poland, in November 7.',
   },
   {
-    'date': 1891,
+    'date': '1891',
     'text': 'Marie went to Paris to study at Sorbonne.',
   },
   {
@@ -12,7 +12,7 @@ export const timeline = [
     'text': 'Earned math and physics degrees at the Sorbonne in Paris. Started writing a doctoral thesis on radiation. It had been discovered in 1896 by Antoine Henri Becquerel.',
   },
   {
-    'date': 1895,
+    'date': '1895',
     'text': 'Married physicist Pierre Curie, head of the laboratory at the School of Industrial Physics and Chemistry in Paris. Joined him in his work there.',
   },
   {
@@ -20,27 +20,27 @@ export const timeline = [
     'text': 'Measured radiation levels emitted by various compounds. Discovered two substances that emitted more radiation than would be expected. In 1898, concluded that one of them contained an unknown radioactive element. Pierre Curie joined his wife in conducting research. Together, the pair discovered two new radioactive elements. They named the elements polonium (after Poland) and radium. Their work then focused on identify their chemical properties.',
   },
   {
-    'date': 1897,
+    'date': '1897',
     'text': 'Gave birth to her first daughter, Irene.',
   },
   {
-    'date': 1903,
+    'date': '1903',
     'text': 'Earned her doctorate, based on the work described in her thesis, Radio-Active Substances. With her husband and Becquerel, was awarded the Nobel Prize in Physics.',
   },
   {
-    'date': 1904,
+    'date': '1904',
     'text': 'Gave birth to her second daughter, Eve. Pierre Curie named a professor at the Sorbonne.',
   },
   {
-    'date': 1906,
+    'date': '1906',
     'text': 'After her husband was killed by a truck on a Paris street, assumed his position at the Sorbonne. Became the first female lecturer in the 750-year history of the university. Also assumed charge of the Sorbonne lab.',
   },
   {
-    'date': 1908,
+    'date': '1908',
     'text': 'Promoted to Professor at the Sorbonne.',
   },
   {
-    'date': 1911,
+    'date': '1911',
     'text': 'Awarded a second Nobel Prize for Chemistry, for isolating pure radium.',
   },
   {
@@ -52,11 +52,11 @@ export const timeline = [
     'text': 'Became director of the scientific department of the Radium Institute. Joined by her daughter Irene Joliot-Curie and husband Frederic Joliot. Continued to research the chemistry of radioactive materials and their medical applications. Lectured internationally. Established scholarships for scientists.',
   },
   {
-    'date': 1934,
+    'date': '1934',
     'text': 'Died of leukemia, likely due to prolonged exposure to radiation during her lifetime.',
   },
   {
-    'date': 1935,
+    'date': '1935',
     'text': 'Daughter Irene Joliot-Curie and Frederic Joliot received the Nobel Prize in Chemistry.',
   },
 ]
