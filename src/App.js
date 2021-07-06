d2a756688f2670ae40eb2cb065e201be88d0d4c7
Elizabeth Vega <eb.vega.m@gmail.com>
2021-07-06 17:46:00 -0400

    rename ids to match test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { timeline } from './timeline';
 import ListItem from './ListItem';
@@ -16,6 +16,12 @@ const darkTheme = {
 function App() {
   const [theme, setTheme] = useState(lightTheme);
   const [btnTheme, setBtnTheme] = useState(darkTheme);
+  const body = document.body;
+
+  useEffect(() => {
+    body.style.background = theme.backgroundColor;
+    body.style.color = theme.color;
+  }, [theme])
 
   const switchTheme = () => {
     theme === lightTheme ? setTheme(darkTheme): setTheme(lightTheme);
@@ -23,20 +29,20 @@ function App() {
   };
 
   return (
-    <div id="container" style={theme}>
+    <div id="main">
       <header id="header-container">
-        <h1>Marie Curie Tribute Page</h1>
+        <h1 id="title">Marie Curie Tribute Page</h1>
         <p>The first woman to win a Nobel Prize. She made many discoveries that led to what we call modern medicine.</p>
-        <button id="theme-button" style={btnTheme} onClick={switchTheme}>Dark</button>
+        <button id="theme-switcher" style={btnTheme} onClick={switchTheme}>Dark</button>
       </header>
       <main id="main-section">
-        <figure id="img-container">
-          <img src="http://www.uncuyo.edu.ar/ices/cache/curie_800_900.jpg" alt="Marie Curie in her laboratory" />
-          <figcaption>
+        <figure id="img-div">
+          <img id="image" src="http://www.uncuyo.edu.ar/ices/cache/curie_800_900.jpg" alt="Marie Curie in her laboratory" />
+          <figcaption id="img-caption">
           Maria Salomea Skłodowska-Curie, known as Marie Curie, poses at the laboratory in Paris.
           </figcaption>
         </figure>
-        <section id="timeline-section">
+        <section id="tribute-info">
           <h3 id="timeline-title">This is a timeline of <span>Marie Curie's</span> life:</h3>
           <ul>
             {timeline.map((el) => <ListItem date={el.date} text={el.text} />)}
@@ -44,7 +50,7 @@ function App() {
           <p>Joliot-Curie’s daughter became a nuclear physicist and their son a biochemist, both making significant scientific contributions. Their grandson is an astrophysicist.</p>
         </section>
         <h3>
-          If you have time, you should read more about this incredible woman on her <span><a href="https://en.wikipedia.org/wiki/Marie_Curie" target="_blank">Wikipedia entry</a></span>.
+          If you have time, you should read more about this incredible woman on her <span><a id="tribute-link" href="https://en.wikipedia.org/wiki/Marie_Curie" target="_blank">Wikipedia entry</a></span>.
         </h3>
       </main>
     </div>
